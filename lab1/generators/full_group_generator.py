25d20adfe0442f65a3a5fc1ad60359b20337c536
VPG1 <malihtorovich@mail.ru>
2024-09-12 21:22:42 +0300

    Full group generator implementation

---
+++ lab1/generators/full_group_generator.py
@@ -0,0 +1,18 @@
+import random
+
+
+class FullGroupGenerator(random.Random):
+    def __init__(self, seed, probability_list):
+        if sum(probability_list) != 1:
+            raise ValueError("Sum of probabilities must be 1")
+        super().__init__(seed)
+        self.dist_function = [sum(probability_list[1:i + 1]) for i in range(len(probability_list))]
+        # self.probability_list = probability_list
+
+    def random(self) -> int:
+        rand_num = super().random()
+        for i in range(len(self.dist_function)):
+            if rand_num <= self.dist_function[i]:
+                return i
+
+        return 0
--- lab1/main.py
+++ lab1/main.py
@@ -3,36 +3,49 @@ import time
 from lab1.generators.simple_generator import SimpleGenerator
 from lab1.generators.complex_generator import ComplexGenerator
 from lab1.generators.complex_dependent_generator import ComplexDependentGenerator
+from lab1.generators.full_group_generator import FullGroupGenerator
 
+# Task1
 a = SimpleGenerator(time.time(), 0.5)
 
 success = 0
-for i in range(0, 10**6):
+for i in range(0, 10 ** 6):
     if a.random():
         success += 1
 
-print(success / 10**6)
+print(success / 10 ** 6)
 
-
+# task 2
 probability_list = [0.3, 0.7]
 b = ComplexGenerator(time.time(), probability_list)
 
 successes = [0] * len(probability_list)
-for _ in range(0, 10**6):
+for _ in range(0, 10 ** 6):
     curRes = b.random()
     for i in range(len(curRes)):
         if curRes[i]:
             successes[i] += 1
 
+print([success / 10 ** 6 for success in successes])
 
-print([success / 10**6 for success in successes])
-
-
+# Task 3
 c = ComplexDependentGenerator(time.time(), 0.6, 0.7)
 print(c.probabilities(0.6, 0.7))
 
 events = [0, 0, 0, 0]
-for _ in range(0, 10**6):
+for _ in range(0, 10 ** 6):
     events[c.random()] += 1
 
-print([el / 10**6 for el in events])
+print([el / 10 ** 6 for el in events])
+
+# Task 4
+group_probability_list = [0.1, 0.2, 0.7]
+print(group_probability_list)
+d = FullGroupGenerator(time.time(), group_probability_list)
+
+events = [0] * len(group_probability_list)
+for _ in range(0, 10 ** 6):
+    event_index = d.random()
+    events[event_index] += 1
+
+print([el / 10 ** 6 for el in events])
